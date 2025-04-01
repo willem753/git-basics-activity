fa8917cb9c3fb8ad45e7f8efd7f2cb072b46b441
willem753 <Willmyers753@gmail.com>
2025-04-01 16:08:37 +1100

    update key base address and reduce leds from 10 to 9

--- Workshop2.v
+++ Workshop2.v
@@ -2,7 +2,7 @@ module Workshop2 (
 	input CLOCK_50,
 	input [1:0] KEY,
 	input [0:0] SW,
-	output [9:0] LEDR
+	output [8:0] LEDR
 	);
 		Hardware u0 (
 		.clk_clk       (CLOCK_50),       //   clk.clk
